62ddc856b76b1af726ecbd116e31fedca4948c9e
Manasa S <srinivassavitha05@gmail.com>
2026-04-16 15:30:16 +0530

    react.js-basics

---
+++ project 6/script.js
@@ -0,0 +1,13 @@
+document.getElementById("getbtn").addEventListener("click", getData);
+
+async function getData(){
+    try{
+    let response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+    let data = await response.json();
+    document.getElementById("status").innerText = data.rates.INR;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+    
